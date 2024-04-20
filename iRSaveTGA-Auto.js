35ce5ded67ee92c6b7938adf669a2e9132e3af5e
leonlvanwyk <30283468+leonlvanwyk@users.noreply.github.com>
2024-04-20 14:34:27 +0200

    Update iRSaveTGA-Auto.js
    Restore the user's active layer afterwards.

--- iRSaveTGA-Auto.js
+++ iRSaveTGA-Auto.js
@@ -3,29 +3,31 @@ var outputFolder = app.activeDocument.path;
 
 if (outputFolder != null) {
 
+    var activeLayer = activeDocument.activeLayer;
+
     // List all top level groups in file
     var allGroups = app.activeDocument.layerSets;
     var len = allGroups.length;
     const groupVisible = [];
 
     // Stores state of Group before setting all groups invisible
-    for (var i = len-1; i >= 0; i--) {
+    for (var i = len - 1; i >= 0; i--) {
         groupVisible[i] = allGroups[i].visible;
         allGroups[i].visible = 0;
     }
 
     // Iterate groups from bottom to top
-    for (var i = len-1; i >= 0; i--) {
+    for (var i = len - 1; i >= 0; i--) {
         var groupName = allGroups[i].name;
-        
+
         // Show Group
         allGroups[i].visible = 1;
-        
+
         // If name start with # skip this layer
         if (groupName[0] == "#") {
             continue;
         }
-        
+
         // Check if it isn't a pop-up
         if (groupName[0] != "^") {
             var master = allGroups[i];
@@ -36,7 +38,7 @@ if (outputFolder != null) {
             var re = /(?!\^\s*).+/;
             groupName = groupName.match(re);
         }
-        
+
         // Ignore red colored groups
         app.activeDocument.activeLayer = allGroups[i];
         if (getLayerColour() != "red") {
@@ -47,34 +49,37 @@ if (outputFolder != null) {
         // Makes groups invisible again just in case of transparent layers like decal layers are being processed
         allGroups[i].visible = 0;
         master.visible = 0;
-    }
 
-    // Reverts Back to Starting State
-    for (var i = len-1; i >= 0; i--) {
-        allGroups[i].visible = groupVisible[i];
+        // Reverts Back to Starting State
+        for (var i = len - 1; i >= 0; i--) {
+            allGroups[i].visible = groupVisible[i];
+        }
+
+        // Restore active layer
+        activeDocument.activeLayer = activeLayer;
     }
 }
 
-
-function SaveTGA(targaFile) {     
-    targaSaveOptions = new TargaSaveOptions();
+// Save as TGA
+function SaveTGA(targaFile) {
+    var targaSaveOptions = new TargaSaveOptions();
     if (getLayerColour() == "violet" || groupName.match(/^.*decal.*$/)) {
-    targaSaveOptions.resolution = TargaBitsPerPixels.THIRTYTWO;
-    targaSaveOptions.alphaChannels = true;
+        targaSaveOptions.resolution = TargaBitsPerPixels.THIRTYTWO;
+        targaSaveOptions.alphaChannels = true;
     } else {
-    targaSaveOptions.resolution = TargaBitsPerPixels.TWENTYFOUR;
-    targaSaveOptions.alphaChannels = false;
+        targaSaveOptions.resolution = TargaBitsPerPixels.TWENTYFOUR;
+        targaSaveOptions.alphaChannels = false;
     }
     targaSaveOptions.rleCompression = true;
     app.activeDocument.saveAs(targaFile, targaSaveOptions, true);
 }
 
 // Return active layer color (found somewere in the internets)
-function getLayerColour(){
-    //Colours returned ....
+function getLayerColour() {
+    // Colours returned ....
     // "none","red","orange","yellowColor","grain","blue","violet","gray"
-    var ref = new ActionReference(); 
-    ref.putEnumerated( charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt") ); 
-    var appDesc = executeActionGet(ref); 
-    return typeIDToStringID(appDesc.getEnumerationValue(stringIDToTypeID('color')) );
+    var ref = new ActionReference();
+    ref.putEnumerated(charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
+    var appDesc = executeActionGet(ref);
+    return typeIDToStringID(appDesc.getEnumerationValue(stringIDToTypeID('color')));
 }
